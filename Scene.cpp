7422577722a2653b5c75154108935e332040a182
atene1084 <atene1084@gmail.com>
2019-04-23 17:55:07 +0900

    k;

--- Scene.cpp
+++ Scene.cpp
@@ -12,31 +12,37 @@
 */
 Scene::Scene(const cahr* name) : name(name);
 {
+	std::cout << "Scene コンストラクタ：" << name << "\n";
 }
 
 Scene::~Scene()
 {
 	Finalize();
+	std::cout << "Scene デストラクタ：" << name << "\n";
 }
 
 void Scene::Play()
 {
 	isActive = true;
+	std::cout << "Scene Play：" << name << "\n";
 }
 
 void Scene::Stop()
 {
 	isActive = false;
+	std::cout << "Scene Stop：" << name << "\n";
 }
 
 void Scene::Show()
 {
 	isVisible = true;
+	std::cout << "Scene Show：" << name << "\n";
 }
 
 void Scene::Hide()
 {
 	isVisible = false;
+	std::cout << "Scene Hide：" << name << "\n";
 }
 
 
